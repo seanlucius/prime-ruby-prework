6f27bbccee3ce718e9a0cd5919247ca304c2e695
Sean Lucius <seanlucius22@icloud.com>
2019-01-17 00:51:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+  number_array = (1..number).to_a
+  dividers = []
+  number_array.each do |value| 
+    if number % value == 0 
+      dividers.push(value)
+    end 
+  end 
+  if number <= 0 || number == 1
+    false 
+    elsif dividers.length > 2 
+    false 
+  else 
+    true 
+  end 
+end 
