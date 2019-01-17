9651ccc93e9e491be01fedbaa759c661672df66a
Sean Lucius <seanlucius22@icloud.com>
2019-01-17 00:44:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 def prime?(number)
-  number_array = (2...number).to_a
+  number_array = (1..number).to_a
+  dividers = []
   number_array.each do |value| 
     if number % value == 0 
-      
+      dividers.push(value)
     end 
   end 
+  
 end 
